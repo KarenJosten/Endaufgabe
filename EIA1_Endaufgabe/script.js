9295073800f341144fcb774f217ba699373c2f18
KarenJosten <39259489+KarenJosten@users.noreply.github.com>
2020-07-27 18:39:44 +0200

    js update

--- EIA1_Endaufgabe/script.js
+++ EIA1_Endaufgabe/script.js
@@ -3,14 +3,15 @@ function playSample(sound) {
     let audios = new Audio(sound);
     audios.play();
 }
-//all Sounds
-var allSounds = ["sound_boing_2.mp3", "sound_boing.mp3", "sound_kids_booo.mp3", "sound_laugh.mp3"];
+/*//ALL SOUNDS
+var allSounds: string[] = ["sound_boing_2.mp3", "sound_boing.mp3", "sound_kids_booo.mp3", "sound_laugh.mp3"];
+
 //play Random sound at the beginning #1
-function soundEasy() {
-    for (let i = 0; i < 3; i++) {
+function soundEasy(): void {
+    for (let i: number = 0; i < 3; i++) {
         sounds.push(allSounds[Math.floor(Math.random() * 3)]);
     }
-}
+}*/
 //Play random sound at beginnig of game #2
 function sounds() {
     var x = Math.floor((Math.random() * 3));
@@ -38,7 +39,6 @@ window.addEventListener("load", function () {
     document.querySelector(".button4").addEventListener("click", function () { playSample("sounds/sound_laugh.mp3"); });
     document.querySelector(".random").addEventListener("click", function () { sounds(); });
 });
-
 //level of difficulty easy/standard/hard
 var speed = 900;
 var time = 400;
@@ -61,3 +61,5 @@ function level() {
         //hard
         document.getElementById(".speed2");
     }
+}
+//# sourceMappingURL=script.js.map
